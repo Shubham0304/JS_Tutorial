9beede71a8876d8d3e70ce665eee9b9798458d39
Shubham0304 <shubhamjoshi0304@gmail.com>
2025-10-19 05:40:00 +0530

    JS Promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -10,3 +10,60 @@ const promiseOne = new Promise(function(resolve, reject){
 promiseOne.then(function() {
     console.log("Promise consumed");
 })
+
+new Promise((resolve,reject) => {
+    setTimeout(function(){
+        console.log("Async Task 2");
+        resolve()
+    }, 1000)
+}).then(()=> {
+    console.log("Async task 2 resolved");
+});
+
+const promiseThree = new Promise(function(resolve, reject) {
+    setTimeout(()=>{
+        resolve({username: "Chai", email: "chai@chai.com"})
+    }, 1000);
+});
+
+promiseThree.then((user)=> {
+console.log(user);
+})
+
+const promiseFour = new Promise((resolve,reject)=> {
+    setTimeout(() => {
+        let error = true;
+        if (!error)
+        {
+            resolve({username: "User2", email: "user2@gmail.com"});
+        }
+        else {
+            reject("Error: Somethig went wrong")
+        }
+             
+    }, 1000);
+})
+
+
+// This is wrong, It will only give you Promise pending when adcessing the username
+// const username = promiseFour.then((user)=> {
+// console.log(user);
+// return user.username;
+// })
+
+// console.log(" User is : ",username);
+
+
+const username = promiseFour
+.then((user)=> {
+console.log(user);
+return user.username;
+})
+.then((username)=> {
+console.log(" User is : ",username);
+})
+.catch(function(error) {
+    console.log(error);
+}).finally(()=> {
+    console.log("The promise either resolve or rejected");
+})
